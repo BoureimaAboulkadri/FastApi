b26cc1092a265815a2f19ba896b2dff451acb76b
ABDOULAKADRI Boureima <diallokader9@gmail.com>
2024-02-28 23:31:12 +0100

    dn_connect

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from pymongo import MongoClient
+from pymongo import MongoClient
+from pymongo import MongoClient
 
+# Configuration FastAPI
 app = FastAPI()
+
+# Configuration MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['NPI']
+collection = db['expressions']
 
 class Expression(BaseModel):
     expression: str
@@ -24,6 +33,8 @@ def calculer_npi(expression):
 def calculer(expression: Expression):
     try:
         resultat = calculer_npi(expression.expression)
-        return {"expression": expression.expression, "resultat": resultat}
+        # Sauvegarder dans MongoDB
+        insertion_result = collection.insert_one({"expression": expression.expression, "resultat": resultat})
+        return {"_id": str(insertion_result.inserted_id), "expression": expression.expression, "resultat": resultat}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
