9c8800ab15bc216d62ad953974a32e1c3955bf3f
ABDOULAKADRI Boureima <diallokader9@gmail.com>
2024-02-28 23:39:35 +0100

    export action

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from pymongo import MongoClient
 from pymongo import MongoClient
 from pymongo import MongoClient
+import pandas as pd
+from io import StringIO
+
 
 # Configuration FastAPI
 app = FastAPI()
@@ -38,3 +41,13 @@ def calculer(expression: Expression):
         return {"_id": str(insertion_result.inserted_id), "expression": expression.expression, "resultat": resultat}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+# Exporter les résultats dans un fichier CSV
+@app.get("/export/")
+def export_csv():
+    cursor = collection.find({})
+    df = pd.DataFrame(list(cursor))
+    del df['_id']
+    csv_string = df.to_csv(index=False)
+    return Response(content=csv_string, media_type="text/csv")
